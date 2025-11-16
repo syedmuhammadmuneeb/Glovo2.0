24e76021f9249f16f7cf493e7d2afbc323005c99
Syed Muhammad Muneeb <67303600+syedmuhammadmuneeb@users.noreply.github.com>
2025-11-16 15:56:45 +0100

    Pushed again
    Changing in folder style

---
+++ Glovo Authentication/Glovo Authentication/Glovo_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glovo_AuthenticationApp.swift
+//  Glovo Authentication
+//
+//  Created by Syed Muhammad Muneeb on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Glovo_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
